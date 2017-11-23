3d4f7fbbde2ddcb2ea954573759a94fe15b16936
Luis Ruiz Pavón <luis_ruiz_pavon@hotmail.com>
2017-11-23 15:03:08 +0100

    Fix build with property name

--- swaggercodegeneration/src/Heroes.Api/Controllers/HeroesController.cs
+++ swaggercodegeneration/src/Heroes.Api/Controllers/HeroesController.cs
@@ -12,16 +12,16 @@ namespace Heroes.Api.Controllers
     {
         private List<Hero> heroes = new List<Hero>
         {
-            new Hero{ Id = 11, Title = "Mr. Nice" },
-            new Hero{ Id = 12, Title = "Narco" },
-            new Hero{ Id = 13, Title = "Bombasto" },
-            new Hero{ Id = 14, Title = "Celeritas" },
-            new Hero{ Id = 15, Title = "Magneta" },
-            new Hero{ Id = 16, Title = "RubberMan" },
-            new Hero{ Id = 17, Title = "Dynama" },
-            new Hero{ Id = 18, Title = "Dr IQ" },
-            new Hero{ Id = 19, Title = "Magma" },
-            new Hero{ Id = 20, Title = "Tornado" }
+            new Hero{ Id = 11, Name = "Mr. Nice" },
+            new Hero{ Id = 12, Name = "Narco" },
+            new Hero{ Id = 13, Name = "Bombasto" },
+            new Hero{ Id = 14, Name = "Celeritas" },
+            new Hero{ Id = 15, Name = "Magneta" },
+            new Hero{ Id = 16, Name = "RubberMan" },
+            new Hero{ Id = 17, Name = "Dynama" },
+            new Hero{ Id = 18, Name = "Dr IQ" },
+            new Hero{ Id = 19, Name = "Magma" },
+            new Hero{ Id = 20, Name = "Tornado" }
         };
 
         [HttpGet]
--- swaggercodegeneration/src/Heroes.Api/Model/Hero.cs
+++ swaggercodegeneration/src/Heroes.Api/Model/Hero.cs
@@ -3,6 +3,6 @@
     public class Hero
     {
         public int Id { get; set; }
-        public string Title { get; set; }
+        public string Name { get; set; }
     }
 }
